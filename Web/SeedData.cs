f821519c96879309c219dd36039c25d2b8b519ba
cs <siwon.cezary@gmail.com>
2023-03-06 20:27:59 +0100

    init

--- Web/SeedData.cs
+++ Web/SeedData.cs
@@ -11,21 +11,6 @@ public static class SeedData
             var provider = scope.ServiceProvider;
             var quizRepo = provider.GetService<IGenericRepository<Quiz, int>>();
             var quizItemRepo = provider.GetService<IGenericRepository<QuizItem, int>>();
-
-            List<QuizItem> items = new List<QuizItem>();
-            items.Add(quizItemRepo.Add(new QuizItem(
-                question: "Litera?",
-                incorrectAnswers: new List<string>() {"B", "C", "D"}, 
-                correctAnswer:"A", id: 1)));
-            items.Add(quizItemRepo.Add(new QuizItem(
-                question: "Planeta?",
-                incorrectAnswers: new List<string>() {"Mars", "Wenus", "Pluton"}, 
-                correctAnswer:"Jowisz", id: 2)));
-            items.Add(quizItemRepo.Add(new QuizItem(
-                question: "Miasto?",
-                incorrectAnswers: new List<string>() {"Kielce", "Kraków", "Katowice"}, 
-                correctAnswer:"Kołobrzeg", id: 3)));
-            quizRepo.Add(new Quiz(id: 1, items: items, title: "Różne"));
         }
     }
 }
