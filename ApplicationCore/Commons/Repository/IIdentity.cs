e48f4deb9561d76d87b2c08ad86dcdb59cd050e6
cs <siwon.cezary@gmail.com>
2023-03-07 17:08:41 +0100

    fix

--- ApplicationCore/Commons/Repository/IIdentity.cs
+++ ApplicationCore/Commons/Repository/IIdentity.cs
@@ -7,4 +7,9 @@ public interface IIdentity<K>: IComparable<K> where K: IComparable<K>
         get;
         set;
     }
+
+    int IComparable<K>.CompareTo(K? other)
+    {
+        return CompareTo(other);
+    }
 }
